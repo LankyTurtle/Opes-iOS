dd85a0193f4e4c464f6a52fd63b3006faaeb5ebf
Keiran Samuels <ksamuels@deloitte.com.au>
2026-08-09 17:50:32 +1000

    feat(features): add empty section views

---
+++ Opes/Features/Accounts/AccountsView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct AccountsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
---
+++ Opes/Features/Budgets/BudgetsView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct BudgetsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}

a6546d99e79f3f6f03877044aec4f3a0978c3541
Keiran Samuels <ksamuels@deloitte.com.au>
2026-08-09 16:11:50 +1000

    refactor(app): reset to blank SwiftUI view

--- Opes/App/OpesApp.swift
+++ Opes/App/OpesApp.swift
@@ -2,16 +2,9 @@ import SwiftUI
 
 @main
 struct OpesApp: App {
-    @StateObject private var session = SessionStore()
-    @StateObject private var accountStore = AccountStore()
-    @StateObject private var dashboardStore = DashboardStore()
-
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environmentObject(session)
-                .environmentObject(accountStore)
-                .environmentObject(dashboardStore)
+            EmptyView()
         }
     }
 }

57ae97f7e7fe373eee4a73a89d511cf0d061bbfb
Keiran Samuels <ksamuels@deloitte.com.au>
2026-08-13 12:43:17 +1000

    fix(build): restore compileable app scaffolding

--- Opes/App/RootView.swift
+++ Opes/App/RootView.swift
@@ -2,10 +2,6 @@ import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        HomeView()
-        AccountsView()
-        TransactionsView()
-        BudgetsView()
-        ProfileView()
+        EmptyView()
     }
 }
--- Opes/Features/Accounts/Account.swift
+++ Opes/Features/Accounts/Account.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct Account: Hashable, Identifiable {
-    
+    let id = UUID()
 }
